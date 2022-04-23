d31807d6e62b401827649f894e207da16adcf939
nan399ana <85630933+nan399ana@users.noreply.github.com>
2022-04-23 12:06:25 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# recognize speech using IBM Speech to Text
+APIKEY = "mPQ6Ur5T9g1f-CNpxPfX3-va_dJC0zwEomMLKXab1RyR"  
+URL = "https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/e2bbe5ff-b11c-4650-81a6-ffad6e132102" 
